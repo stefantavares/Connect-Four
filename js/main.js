e5f106c591aff1775af696d750b30c21b3b83b1d
Stefan Tavares <stefantavares@Stefans-MacBook-Pro-2.local>
2021-11-22 09:21:48 -0800

    Constants added

--- js/main.js
+++ js/main.js
@@ -1,7 +1,12 @@
 /*----- constants -----*/
+const redCircleImg = "https://www.iconsdb.com/icons/preview/soylent-red/circle-xxl.png";
+const blueCircleImg = "https://www.iconsdb.com/icons/preview/royal-blue/circle-xxl.png";
 
-
-
+const lookup = {
+  1: 'red',
+  '-1': 'blue',
+  null: 'white',
+};
 
 /*----- app's state (variables) -----*/
 
@@ -18,7 +23,7 @@
 
 
 
-/*----- functions -----*/
+/*----- functions -----*/ 
 
 
 
