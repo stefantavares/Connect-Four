806b9bcf0270956fc1452a6671e31ad1a227be92
Stefan Tavares <stefantavares@Stefans-MacBook-Pro-2.local>
2021-11-23 09:09:37 -0800

    handle move function added

--- js/main.js
+++ js/main.js
@@ -2,6 +2,7 @@
 const redCircleImg = "https://www.iconsdb.com/icons/preview/soylent-red/circle-xxl.png";
 const blueCircleImg = "https://www.iconsdb.com/icons/preview/royal-blue/circle-xxl.png";
 
+
 const lookup = {
   1: 'red',
   '-1': 'blue',
@@ -17,6 +18,7 @@ let winner;
 /*----- cached element references -----*/
 const imgEl = document.querySelector('img');
 const slotEls = document.querySelectorAll('td');
+const tableRow = document.getElementsByTagName('tr');
 const displayMessage = document.querySelector('h3');
 
 
@@ -29,7 +31,30 @@ document.querySelector('button').addEventListener('click', init);
 /*----- functions -----*/ 
 init();
 
-function handleMove() {
+
+function handleMove(e) {
+  let col = e.target.cellIndex;
+  let row = [];
+
+  for (let i = 5; i > -1; i--) {
+    if (tableRow[i].children[col].style.background === 'white') {
+      row.push(tableRow[i].children[col]);
+      if (turn === 1) {
+        row[0].style.background = 'red';
+        return turn = -1;
+      }
+      if (turn === -1) {
+        row[0].style.background = 'blue';
+        return turn = 1;
+      }
+    }
+  }
+  render();
+}
+
+
+
+function checkWinner() {
 
 }
 
@@ -42,16 +67,26 @@ function render() {
 }
 
 function init() {
-  board = 
-    [null, null, null, null, null, null, null, 
+  board = [
     null, null, null, null, null, null, null, 
     null, null, null, null, null, null, null, 
     null, null, null, null, null, null, null, 
     null, null, null, null, null, null, null, 
     null, null, null, null, null, null, null, 
     null, null, null, null, null, null, null, 
-    ];
+  ];
   turn = 1;
   winner = null;
   render(); 
 }
+
+
+
+// function handleMove(e) {
+//   const idx = parseInt(e.target.id.replace('slt', ''));
+//   if (board[idx] !== null) return;
+//   board[idx] = turn;
+//   turn *= -1;
+//   winner = checkWinner();
+//   render();
+// }
