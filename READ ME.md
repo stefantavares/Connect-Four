7e4ff5f168107a4947d6e2f62aee48a7c71f0afe
Stefan Tavares <stefantavares@Stefans-MacBook-Pro-2.local>
2021-11-28 19:58:26 -0800

    Display turn/winner updated. README added

---
+++ READ ME.md
@@ -0,0 +1,28 @@
+# **CONNECT FOUR**
+
+## **HISTORY**
+
+It was in 1974 when Milton Bradley brought the plastic version of Connect Four to the masses creating a cultural phenomenon. Howard Wexler, a toy inventor, is given credit for creating the Connect Four we all grew up playing. Milton Bradly was eventually bought out by Hasbro who continues to sell the game.
+<br />
+<br />
+
+## **HOW TO PLAY**
+
+This is a two player game. Player 1 being the color **Red** and Player 2 being the color **Blue**. The board begins empty, and red is up first! The players will click on the column in which they want to drop their piece into the next available vertical slot in that column. Take turns until either **Red** or **Blue** has connected 4 pieces either vertically, horizontally, or diagonally. If the board fills up with neither player connecting 4, the game is a tie!
+<br />
+<br />
+
+## **TECHNOLOGIES USED**
+
+- HTML
+- CSS
+- JAVASCRIPT
+  <br />
+  <br />
+
+### **NEXT STEPS:**
+
+Some planned future enhancements are:
+
+- Highlight the winning combo when there's a win.
+- Score tracker between players.
--- js/main.js
+++ js/main.js
@@ -1,12 +1,13 @@
 // /*----- constants -----*/
-
-// const circleImgs = {
-//   1: "https://www.iconsdb.com/icons/preview/soylent-red/circle-xxl.png",
-//   '-1': "https://www.iconsdb.com/icons/preview/royal-blue/circle-xxl.png"
-// }
-
 const redCircleImg = "https://www.iconsdb.com/icons/preview/soylent-red/circle-xxl.png";
 const blueCircleImg = "https://www.iconsdb.com/icons/preview/royal-blue/circle-xxl.png";
+const whiteCircleImg = "https://www.iconsdb.com/icons/preview/white/circle-xxl.png";
+
+const circleImgs = {
+  1: redCircleImg,
+  '-1': blueCircleImg,
+  null: whiteCircleImg,
+};
 
 const lookup = {
   1: 'red',
@@ -147,15 +148,17 @@ function checkWinner() {
 function render() {
   board.forEach(function(sl, i) {
     slotEls[i].style.background = lookup[sl];
-    return turn === 1 ? imgEl.src = redCircleImg : imgEl.src = blueCircleImg;
   });
   if (winner === 'T'){
     displayMessage.innerHTML = `LOOK AT THAT... IT'S A TIE!`;
+    imgEl.src = circleImgs.null;
   } else if (winner) {
     displayMessage.innerHTML = `${lookup[winner].toUpperCase()} WINS!`;
     document.querySelector('table').removeEventListener('click', handleMove);
+    imgEl.src = circleImgs[winner];
   } else {
-    displayMessage.innerHTML = `IT'S YOUR TURN ${lookup[turn].toUpperCase()}`;
+    displayMessage.innerHTML = `IT'S YOUR TURN:`;
+    imgEl.src = circleImgs[turn];
   }
 }
 
